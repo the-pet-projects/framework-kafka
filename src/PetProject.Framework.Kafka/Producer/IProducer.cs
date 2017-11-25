937c77c7ad168c71ba6664aaa858735a4c4cc7c9
Pedro Moreira <pedromsmoreira51@gmail.com>
2017-11-25 22:25:01 +0000

    add missing function to IProducer interface

--- src/PetProject.Framework.Kafka/Producer/IProducer.cs
+++ src/PetProject.Framework.Kafka/Producer/IProducer.cs
@@ -2,10 +2,13 @@
 {
     using System;
     using System.Threading.Tasks;
+    using Confluent.Kafka;
     using Topics;
 
     internal interface IProducer<TTopic> : IDisposable
     {
         Task Produce<TMessage>(TMessage message) where TMessage : IMessageContract;
+
+        Task<Message<string, string>> ProduceAsync<TMessage>(TMessage message) where TMessage : IMessageContract;
     }
 }
